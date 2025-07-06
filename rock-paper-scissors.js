c53fdbdf8c8f684798afa4fc56db25923c3aaf9b
Daniel Hodiamont <213696653+ygsitaa@users.noreply.github.com>
2025-07-07 00:27:03 +0200

    step 5 + fixes to my code because i got confused

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,17 +4,53 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoiceResult = Math.random();
-    if (computerChoiceResult <= 0.33) {
-        return "Rock !";
-    } else if (computerChoiceResult <= 0.66) {
-        return "Paper !";
+    if (Math.random() <= 0.33) {
+        return "rock";
+    } else if (Math.random() <= 0.66) {
+        return "paper";
     } else {
-        return "Scissors !";
+        return "scissors";
     }
 }
 
 function getHumanChoice () {
-    let humanChoiceResult = prompt("Rock, Paper or Scissors ?");
-    return humanChoiceResult;
+    let humanResult = prompt("Rock, Paper or Scissors ?");
+    if (humanResult === "rock") {
+        return "rock";
+    } else if (humanResult === "paper") {
+        return "paper";
+    } else if (humanResult === "scissors") {
+        return "scissors";
+    }
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+        humanScore++;
+        console.log("You win, Rock beats Scissors !");
+    } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
+        computerChoice++;
+        console.log("You lost, Paper beats Rock !");
+    } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+        humanScore++;
+        console.log("You win, Paper beats Rock !");
+    } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+        computerChoice++;
+        console.log("You lost, Scissors beats Paper !");
+    } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+        humanScore++;
+        console.log("You win, Scissors beats Paper !");
+    } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+        computerChoice++;
+        console.log("You lost, Rock beats Scissors !");
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a tie !");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
